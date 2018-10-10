533b63e6fc0cd15b24bb7cdc1b24d9bf9e797375
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-10-10 19:45:24 +0200

    Add files via upload

---
+++ PruebaPunto.java
@@ -0,0 +1,12 @@
+public class PruebaPunto {
+    public static void main(String[] args) {
+
+        Punto p = new Punto();
+        Punto q = new Punto(1, 2);
+        Punto r = new Punto(5, 8);
+        System.out.println(q.distancia(r));
+
+
+    }
+
+}
---
+++ Punto.java
@@ -0,0 +1,37 @@
+public class Punto {
+
+    private double x;
+    private double y;
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Punto() {
+        this(0, 0);
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distancia(Punto punto) {
+        return Math.sqrt(Math.pow(this.x - punto.x, 2) + Math.pow(this.y - punto.y, 2));
+    }
+
+
+}
